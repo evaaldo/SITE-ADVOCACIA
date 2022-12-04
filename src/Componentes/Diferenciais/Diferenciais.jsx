a8018eea9ac5bec58e5c1387688d58ed7542b5a2
evaaldo <evaaldormj@gmail.com>
2022-12-04 20:25:19 -0300

    Criação da versão mobile da aba diferenciais

--- src/Componentes/Diferenciais/Diferenciais.jsx
+++ src/Componentes/Diferenciais/Diferenciais.jsx
@@ -11,39 +11,77 @@ export default function Diferenciais() {
 
     return(
         <div className='diferenciais'>
-            <h4 className='diferenciais__subtitulo'>Motivos para nos escolher</h4>
-            <h1 className='diferenciais__titulo'>Alguns diferenciais</h1>
-            <div className='diferenciais__itens'>
-                <div className='diferenciais__item'>
-                    <div className='item'>
-                        <RiMedalFill size={30}/>
-                        <h4 className='diferenciais__subtitulo'>Formação qualificada</h4>
+            <div className='diferenciais-desktop'>
+                <h4 className='diferenciais__subtitulo'>Motivos para nos escolher</h4>
+                <h1 className='diferenciais__titulo'>Alguns diferenciais</h1>
+                <div className='diferenciais__itens'>
+                    <div className='diferenciais__item'>
+                        <div className='item'>
+                            <RiMedalFill size={30}/>
+                            <h4 className='diferenciais__subtitulo'>Formação qualificada</h4>
+                        </div>
+                        <div className='item'>
+                            <RiStarFill size={30}/>
+                            <h4 className='diferenciais__subtitulo'>Casos de sucesso</h4>
+                        </div>
+                        <div className='item'>
+                            <RiUser3Fill size={30}/>
+                            <h4 className='diferenciais__subtitulo'>Zelo e cuidado</h4>
+                        </div>
                     </div>
-                    <div className='item'>
-                        <RiStarFill size={30}/>
-                        <h4 className='diferenciais__subtitulo'>Casos de sucesso</h4>
-                    </div>
-                    <div className='item'>
-                        <RiUser3Fill size={30}/>
-                        <h4 className='diferenciais__subtitulo'>Zelo e cuidado</h4>
+                    <div className='diferenciais__item'>
+                        <div className='item'>
+                            <FaClock size={30}/>
+                            <h4 className='diferenciais__subtitulo'>Rápido atendimento</h4>
+                        </div>
+                        <div className='item'>
+                            <GrPersonalComputer size={30}/>
+                            <h4 className='diferenciais__subtitulo'>On-line ou presencial</h4>
+                        </div>
+                        <div className='item'>
+                            <BsHeartFill size={30}/>
+                            <h4 className='diferenciais__subtitulo'>Amamos direito</h4>
+                        </div>
                     </div>
                 </div>
-                <div className='diferenciais__item'>
-                    <div className='item'>
-                        <FaClock size={30}/>
-                        <h4 className='diferenciais__subtitulo'>Rápido atendimento</h4>
+                <div className='linha linhaequipe'></div>
+            </div>
+            <div className='diferenciais-mobile'>
+                <div className='diferenciais-mobile__titulos'>
+                    <h4 className='diferenciais-mobile__subtitulo'>Motivos para nos escolher</h4>
+                    <h1 className='diferenciais-mobile__titulo'>Alguns diferenciais</h1>
+                </div>
+                <div className='diferenciais__itens'>
+                    <div className='diferenciais__item'>
+                        <div className='item'>
+                            <RiMedalFill size={30}/>
+                            <h4 className='diferenciais__subtitulo'>Formação qualificada</h4>
+                        </div>
+                        <div className='item'>
+                            <RiStarFill size={30}/>
+                            <h4 className='diferenciais__subtitulo'>Casos de sucesso</h4>
+                        </div>
+                        <div className='item'>
+                            <RiUser3Fill size={30}/>
+                            <h4 className='diferenciais__subtitulo'>Zelo e cuidado</h4>
+                        </div>
                     </div>
-                    <div className='item'>
-                        <GrPersonalComputer size={30}/>
-                        <h4 className='diferenciais__subtitulo'>On-line ou presencial</h4>
-                    </div>
-                    <div className='item'>
-                        <BsHeartFill size={30}/>
-                        <h4 className='diferenciais__subtitulo'>Amamos direito</h4>
+                    <div className='diferenciais__item'>
+                        <div className='item'>
+                            <FaClock size={30}/>
+                            <h4 className='diferenciais__subtitulo'>Rápido atendimento</h4>
+                        </div>
+                        <div className='item'>
+                            <GrPersonalComputer size={30}/>
+                            <h4 className='diferenciais__subtitulo'>On-line ou presencial</h4>
+                        </div>
+                        <div className='item'>
+                            <BsHeartFill size={30}/>
+                            <h4 className='diferenciais__subtitulo'>Amamos direito</h4>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className='linha linhaequipe'></div>
         </div>
     )
 
