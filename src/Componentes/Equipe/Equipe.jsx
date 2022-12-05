63766c0ad56ad6688084ad4202d617652ba9793d
evaaldo <evaaldormj@gmail.com>
2022-12-04 21:31:06 -0300

    Criação da versão mobile da aba de equipe

--- src/Componentes/Equipe/Equipe.jsx
+++ src/Componentes/Equipe/Equipe.jsx
@@ -6,13 +6,26 @@ export default function Equipe() {
 
     return(
         <div className="equipe">
-            <h1 className='equipe__titulo'>Nossa equipe</h1>
-            <div className='equipe__itens'>
-                <div className='equipe__item'>
-                    <img className='equipe__imagem' src={usuario}></img>
-                    <h4 className='equipe__nome'>Fulano de Tal</h4>
-                    <p className='equipe__cargo'>ADVOGADA</p>
-                    <p className='equipe__descricao'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+            <div className='equipe-desktop'>
+                <h1 className='equipe__titulo'>Nossa equipe</h1>
+                <div className='equipe__itens'>
+                    <div className='equipe__item'>
+                        <img className='equipe__imagem' src={usuario}></img>
+                        <h4 className='equipe__nome'>Fulano de Tal</h4>
+                        <p className='equipe__cargo'>ADVOGADA</p>
+                        <p className='equipe__descricao'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='equipe-mobile'>
+                <h1 className='equipe__titulo'>Nossa equipe</h1>
+                <div className='equipe__itens'>
+                    <div className='equipe__item'>
+                        <img className='equipe__imagem' src={usuario}></img>
+                        <h4 className='equipe__nome'>Fulano de Tal</h4>
+                        <p className='equipe__cargo'>ADVOGADA</p>
+                        <p className='equipe__descricao'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                    </div>
                 </div>
             </div>
         </div>
